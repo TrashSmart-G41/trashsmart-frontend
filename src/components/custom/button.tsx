6d42d87012fa848e1c724956f7fa227fc9bb0d04
Lasith Ranahewa <lasithmrana@gmail.com>
2024-07-23 08:50:45 +0530

    tabs

--- src/components/custom/button.tsx
+++ src/components/custom/button.tsx
@@ -21,12 +21,14 @@ const buttonVariants = cva(
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
         nav_selected: 'bg-primary/15 text-primary',
+        scale_btn: 'bg-primary rounded text-accent-foreground text-[12px]',
       },
       size: {
         default: 'h-9 px-4 py-2',
         sm: 'h-8 rounded-md px-3 text-xs',
         lg: 'h-10 rounded-md px-8',
         icon: 'h-9 w-9',
+        scale_btn: 'p-2 px-4 rounded-full',
       },
     },
     defaultVariants: {
