f29882e2b02817b946dc76dc6be5fd6a9edfd359
Masha <mashawickramasinghe@Mashas-MacBook-Pro.local>
2024-11-15 13:57:41 +0530

    Organization div of contractor dashboard

--- src/pages/dashboard/org/columns.tsx
+++ src/pages/dashboard/org/columns.tsx
@@ -4,37 +4,15 @@ import { useNavigate } from 'react-router-dom'
 // import { Badge } from '@/components/ui/badge'
 // import { Checkbox } from '@/components/ui/checkbox'
 import { DataTableColumnHeader } from './data-table-column-header'
-import { DataTableRowActions } from './data-table-row-actions'
+//import { DataTableRowActions } from './data-table-row-actions'
 
 // import { statuses, regions } from '../data/data'
 import { Organization } from '../org/data/schema'
 import { Button } from '@/components/custom/button'
+import { EditOrganization } from '/Users/mashawickramasinghe/Desktop/UCSC/3rd Year/Group Project 2/trashsmart-frontend/src/pages/organizations/components/edit-organization-form.tsx'
+import { DeleteOrganization } from '/Users/mashawickramasinghe/Desktop/UCSC/3rd Year/Group Project 2/trashsmart-frontend/src/pages/organizations/components/delete-organization.tsx'
 
 export const columns: ColumnDef<Organization>[] = [
-  // {
-  //   id: 'select',
-  //   header: ({ table }) => (
-  //     <Checkbox
-  //       checked={
-  //         table.getIsAllPageRowsSelected() ||
-  //         (table.getIsSomePageRowsSelected() && 'indeterminate')
-  //       }
-  //       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-  //       aria-label='Select all'
-  //       className='translate-y-[2px]'
-  //     />
-  //   ),
-  //   cell: ({ row }) => (
-  //     <Checkbox
-  //       checked={row.getIsSelected()}
-  //       onCheckedChange={(value) => row.toggleSelected(!!value)}
-  //       aria-label='Select row'
-  //       className='translate-y-[2px]'
-  //     />
-  //   ),
-  //   enableSorting: false,
-  //   enableHiding: false,
-  // },
   {
     accessorKey: 'id',
     header: ({ column }) => (
@@ -49,7 +27,7 @@ export const columns: ColumnDef<Organization>[] = [
     enableHiding: false,
   },
   {
-    accessorKey: 'name',
+    accessorKey: 'firstName',
     header: ({ column }) => (
       <DataTableColumnHeader
         column={column}
@@ -57,7 +35,7 @@ export const columns: ColumnDef<Organization>[] = [
         className='text-center text-[14px]'
       />
     ),
-    cell: ({ row }) => <div>{row.getValue('name')}</div>,
+    cell: ({ row }) => <div>{row.getValue('firstName')}</div>,
     enableSorting: true,
     enableHiding: false,
   },
@@ -90,7 +68,7 @@ export const columns: ColumnDef<Organization>[] = [
     // enableSorting: true,
   },
   {
-    accessorKey: 'waste_volume',
+    accessorKey: 'totalWaste',
     header: ({ column }) => (
       <DataTableColumnHeader
         className='text-[14px]'
@@ -98,44 +76,19 @@ export const columns: ColumnDef<Organization>[] = [
         title='Waste Volume'
       />
     ),
-    cell: ({ row }) => <div>{row.getValue('waste_volume')}</div>,
+    cell: ({ row }) => <div>{row.getValue('totalWaste')}</div>,
     // enableSorting: true,
   },
-
-  // {
-  //   id: 'actions',
-  //   header: () => null,
-  //   cell: ({ row }: { row: { getValue: (key: string) => string; employee_id?: string } }) => {
-  //     const navigate = useNavigate()
-
-  //     const handleButtonClick = () => {
-  //       navigate(`/cleaners/${row.getValue('employee_id')}`)
-  //     }
-
-  //     return (
-  //       <div className='mr-4 text-right'>
-  //         <Button
-  //           variant='ghost'
-  //           className='flex h-8  px-2 text-[12px] text-primary/80 hover:text-primary'
-  //           onClick={handleButtonClick}
-  //         >
-  //           View
-  //         </Button>
-  //       </div>
-  //     )
-  //   },
-  //   enableSorting: false,
-  //   enableHiding: false,
-  // },
   {
     id: 'actions',
     cell: ({ row }) => {
       const navigate = useNavigate()
-
+      const contId = String(row.getValue('id') || '').slice(-3)
+  
       const handleButtonClick = () => {
         navigate(`/organizations/${row.getValue('id')}`)
       }
-
+  
       return (
         <div className='mr-4 flex items-center justify-end'>
           <Button
@@ -145,9 +98,12 @@ export const columns: ColumnDef<Organization>[] = [
           >
             View
           </Button>
-          <DataTableRowActions row={row} />
+          {/* <DataTableRowActions row={row} /> */}
+          <EditOrganization contId={contId} />
+          <DeleteOrganization contId={contId} />
         </div>
       )
     },
   },
+  
 ]
